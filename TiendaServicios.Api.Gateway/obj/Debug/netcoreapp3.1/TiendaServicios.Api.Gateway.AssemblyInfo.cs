4764922ad582269e34c8f0aefa3a2ac3cadfa4be
Walter Arce <walterarce@gmail.com>
2020-11-01 10:04:05 -0300

    se incorpora gateway

--- TiendaServicios.Api.Gateway/obj/Debug/netcoreapp3.1/TiendaServicios.Api.Gateway.AssemblyInfo.cs
+++ TiendaServicios.Api.Gateway/obj/Debug/netcoreapp3.1/TiendaServicios.Api.Gateway.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TiendaServicios.Api.Libro")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TiendaServicios.Api.Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("TiendaServicios.Api.Libro")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TiendaServicios.Api.Libro")]
+[assembly: System.Reflection.AssemblyProductAttribute("TiendaServicios.Api.Gateway")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TiendaServicios.Api.Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
--- TiendaServicios.Api.Gateway/obj/Debug/netcoreapp3.1/TiendaServicios.Api.Gateway.MvcApplicationPartsAssemblyInfo.cs
+++ TiendaServicios.Api.Gateway/obj/Debug/netcoreapp3.1/TiendaServicios.Api.Gateway.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TiendaServicios.Api.Libro")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("TiendaServicios.Api.Libro")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TiendaServicios.Api.Libro")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Ocelot")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
